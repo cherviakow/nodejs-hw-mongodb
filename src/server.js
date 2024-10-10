12c8d57769d7192bcd33febc7429b208a56e228b
Ihor Cherviakov <118768294+cherviakow@users.noreply.github.com>
2024-10-10 02:15:17 -0400

    new update

--- src/server.js
+++ src/server.js
@@ -35,14 +35,14 @@ app.get('/contacts/:contactId', async (req, res) => {
     const {contactId} = req.params;
 
     try{
-        const contactsId = await getContactById(contactId);
-        if (!contactId) {
-            return res.status(404).json('Contact not found');
+        const contact = await getContactById(contactId);
+        if (!contact) {
+            return res.status(404).json({message: 'Contact not found'});
         }
         res.status(200).json({
             status: 200,
             message: `Successfully found contact with id ${contactId}!`,
-            data: contactsId,
+            data: contact,
     });
     } catch(error) {
         res.status(500).json({
