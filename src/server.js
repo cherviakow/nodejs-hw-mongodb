b184e433185848c0447ac7ea18e829255e3b9e26
Ihor Cherviakov <118768294+cherviakow@users.noreply.github.com>
2024-10-14 23:44:28 -0400

    new update

--- src/server.js
+++ src/server.js
@@ -3,58 +3,26 @@ import cors from 'cors';
 import pino from 'pino-http';
 import { env } from './utils/.env.js';
 import dotenv from 'dotenv';
-import { getAllContacts, getContactById } from './serviceContacts/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
+
 dotenv.config();
+
+import contactRoutes from './routes/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 export async function setupServer() {
   const app = express();
   const PORT = Number(env('PORT', '3000'));
-  app.use(express.json());
+//   app.use(express.json());
 
   app.use(cors());
   app.use(pino({ transport: { target: 'pino-pretty' } }));
-
-  app.get('/contacts', async (req, res) => {
-    try {
-      const contacts = await getAllContacts();
-      res.status(200).json({
-        status: 200,
-        message: 'Found all contacts',
-        data: contacts,
-      });
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  });
-
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-
-    try {
-      const contact = await getContactById(contactId);
-      if (!contact) {
-        return res.status(404).json({ message: 'Contact not found' });
-      }
-      res.status(200).json({
-        status: 200,
-        message: `Successfully found contact with id ${contactId}!`,
-        data: contact,
-      });
-    } catch (error) {
-      res.status(500).json({
-        message: 'Server error',
-        error: error.message,
-      });
-    }
-  });
-
+  app.use(contactRoutes);
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 
-  app.use((req, res, next) => {
-    res.status(404).json({
-      message: 'Not found!',
-    });
-  });
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 }
--- src/serviceContacts/contacts.js
+++ src/serviceContacts/contacts.js
@@ -1,9 +1,21 @@
-import { Contact } from "../models/contact.js";
+import { Contact } from '../models/contact.js';
 
 export const getAllContacts = async () => {
-    return await Contact.find();
+  return await Contact.find();
 };
 
 export const getContactById = async (contactId) => {
-    return await Contact.findById(contactId);
+  return await Contact.findById(contactId);
+};
+
+export function createContact(contact) {
+  return Contact.create(contact);
+}
+
+export function deleteContact(id) {
+  return Contact.findByIdAndDelete(id);
+};
+
+export function changeContactType(contactId, newContact) {
+  return Contact.findByIdAndUpdate(contactId, newContact, { new: true });
 };
